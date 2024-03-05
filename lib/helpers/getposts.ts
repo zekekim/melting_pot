6b023347e39fb653fcaf30a3d61a0942ab69658f
Daniel Teo <dteo318@gmail.com>
2024-03-04 19:43:30 -0800

    Account page displays users posts

--- lib/helpers/getposts.ts
+++ lib/helpers/getposts.ts
@@ -27,3 +27,30 @@ export async function getPosts(): Promise<PostWithRecipe[]> {
     return []
 }
 
+export async function getUserPosts(): Promise<PostWithRecipe[]> {
+    try {
+        const user = await validateRequest()
+        if (!user || user.user === null) {
+            throw Error("no user")
+        }
+        const posts: PostWithRecipe[] = await db.post.findMany(
+            {
+                where: {
+                    user: user.user,
+                },
+                include: {
+                    recipe: {
+                        include:
+                            { ingredients: true }
+                    },
+                    likes: true,
+                    replies: true
+                }
+            })
+        return posts
+
+    } catch (e) {
+    }
+    return []
+}
+
