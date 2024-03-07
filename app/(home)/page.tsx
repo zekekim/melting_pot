e74eb75b02cdeee809395634acdede4ac2c25887
Daniel Teo <dteo318@gmail.com>
2024-03-06 20:24:13 -0800

    Fixed hydration error

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,20 @@
 import { PostWithRecipe } from "@/lib/types";
 import RecipeFeed from "@/components/recipefeed";
 import { getPosts } from "@/lib/helpers/getposts";
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const { user } = await validateRequest();
+  if (!user) {
+    return redirect("/signup");
+  }
   const recipes: Array<PostWithRecipe> = await getPosts();
 
   return (
     <div className="grid grid-cols-4 gap-20">
       <div className="flex flex-col col-span-3 ">
-        <RecipeFeed posts={recipes} />
+        <RecipeFeed posts={recipes} userId={user.id} />
       </div>
       <div>
         {/* For recommmended events */}
